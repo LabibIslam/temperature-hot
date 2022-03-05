ddf4512bc9eb93f2777ee8fbb0cd18823300025f
LabibIslam <labib.islam.practice@gmail.com>
2022-03-05 20:52:03 +0600

    besic weather checker

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const API_KEY = `21d86be831c0490224092ea0d4732e65`
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data));
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url)
+    console.log(temperature);
+}
+
